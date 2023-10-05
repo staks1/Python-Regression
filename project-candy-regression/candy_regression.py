65499a3e0844a6a4d0ca78b53c074a4afa579aff
staks1 <sta8srules@gmail.com>
2023-10-05 17:44:14 +0300

    - Updates on Dataset Analysis

--- project-candy-regression/candy_regression.py
+++ project-candy-regression/candy_regression.py
@@ -8,6 +8,7 @@ Created on Wed Oct  4 12:21:45 2023
 import pandas as pd
 import numpy as np 
 import matplotlib.pyplot as plt
+import seaborn as sns
 
 # read dataset 
 data = pd.read_csv("./candy-data.csv",header=0)
@@ -31,3 +32,27 @@ categorical_data = data[['chocolate','fruity','caramel','peanutyalmondy','nougat
 continuous_data = data[['sugarpercent','pricepercent','winpercent']]
 
 
+
+# we will now study the distribution of the data 
+plt.figure()
+sns.pairplot(data = continuous_data,diag_kind='kde')
+
+# we can see that the continuous features seem to follow a distribution 
+# close to Gauss
+
+plt.figure()
+vp = sns.violinplot(data = categorical_data)
+vp.set_xticklabels(vp.get_xticklabels(), rotation=90)
+
+# compute correlation of columns (we will exclude the competitornames)
+# we put all the columns together (predictors)
+
+# from a quick correlation analysis
+# we can make some potential observations about the relationship between the different features 
+
+# linear 
+all_data = data.iloc[:,1:]
+linear_correlations = all_data.corr(method = 'pearson')
+
+# non-linear 
+non_linear_correlations = all_data.corr(method = 'spearman')
